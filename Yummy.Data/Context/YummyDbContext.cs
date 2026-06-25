915a8d02e2d1b613fcf883232301ce658372e8fc
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-25 13:55:07 +0300

    DbContext sınıfı tamamlandı ve migrationa hazır hale getirildi.

--- Yummy.Data/Context/YummyDbContext.cs
+++ Yummy.Data/Context/YummyDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Yummy.Entity;
@@ -13,6 +14,69 @@ namespace Yummy.Data.Context
     {
         public YummyDbContext(DbContextOptions<YummyDbContext> options) : base(options)
         {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Chef> Chefs { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Gallery> Galleries { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+
+            #region Identity
+            builder.Entity<AppUser>().ToTable("Users");
+            builder.Entity<IdentityRole<Guid>>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<Guid>>().ToTable("UserRoles");
+            builder.Entity<IdentityUserClaim<Guid>>().ToTable("UserClaims");
+            builder.Entity<IdentityUserLogin<Guid>>().ToTable("UserLogins");
+            builder.Entity<IdentityRoleClaim<Guid>>().ToTable("RoleClaims");
+            builder.Entity<IdentityUserToken<Guid>>().ToTable("UserTokens");
+            #endregion
+
+            #region Pricing
+            builder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+            #endregion
+
+            #region Fluent API
+
+            builder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade); 
+
+            builder.Entity<Message>()
+                .HasOne(m => m.AppUser)
+                .WithMany(u => u.Messages)
+                .HasForeignKey(m => m.AppUserId)
+                .IsRequired() 
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Reservation>()
+                .HasOne(r => r.AppUser)
+                .WithMany(u => u.Reservations)
+                .HasForeignKey(r => r.AppUserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Testimonial>()
+                .HasOne(t => t.AppUser)
+                .WithMany(u => u.Testimonials)
+                .HasForeignKey(t => t.AppUserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+            #endregion
         }
     }
 }
