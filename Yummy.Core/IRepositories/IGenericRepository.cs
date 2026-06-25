c318dc77cf011f0329951bcf5bcb106d8ddfa5c2
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-25 14:06:59 +0300

    IGenericRepository ve GenericRepository sınıfları oluşturuldu, içerisinde metotlar tanımlandı.

---
+++ Yummy.Core/IRepositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Core.IRepositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetWhereAsync(Expression<Func<T, bool>> predicate);
+        Task<T?> GetByIdAsync(Guid id);
+        Task<T?> GetSingleAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        IQueryable<T> GetAsQueryable();
+    }
+}
---
+++ Yummy.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Yummy.Core.IRepositories;
+using Yummy.Data.Context;
+
+namespace Yummy.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly YummyDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(YummyDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() =>
+            await _dbSet.ToListAsync();
+
+        public async Task<IEnumerable<T>> GetWhereAsync(Expression<Func<T, bool>> predicate) =>
+            await _dbSet.Where(predicate).ToListAsync();
+
+        public async Task<T?> GetByIdAsync(Guid id) =>
+            await _dbSet.FindAsync(id);
+
+        public async Task<T?> GetSingleAsync(Expression<Func<T, bool>> predicate) =>
+            await _dbSet.FirstOrDefaultAsync(predicate);
+
+        public async Task AddAsync(T entity) =>
+            await _dbSet.AddAsync(entity);
+
+        public void Update(T entity) =>
+            _dbSet.Update(entity);
+
+        public void Remove(T entity) =>
+            _dbSet.Remove(entity);
+
+        public IQueryable<T> GetAsQueryable() => _dbSet.AsQueryable();
+    }
+}
