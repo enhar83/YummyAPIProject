82fccf939594579cf387501834ef745962bea234
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-25 11:23:52 +0300

    Service entiysi oluşturuldu içerisinde propertyler tanımlandı.

---
+++ Yummy.Entity/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Entity
+{
+    public class Service
+    {
+        public Guid ServiceId { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string IconUrl { get; set; } = null!;
+    }
+}
