da5539c2515b991feb57145fe00ddbfa6ea475b2
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-25 11:27:51 +0300

    Testiomonial entitysi eklendi içerisinde propertyler tanımlandı.

---
+++ Yummy.Entity/Testimonial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Entity
+{
+    public class Testimonial
+    {
+        public Guid TestimonialId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Comment { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+    }
+}
