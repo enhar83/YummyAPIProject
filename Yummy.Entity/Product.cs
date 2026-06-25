15f2ba8d10165e397773927a8ce68178e4c29ab7
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-25 11:26:15 +0300

    Product entitysi oluşturuldu içerisinde propertyler tanımlandı.

---
+++ Yummy.Entity/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Entity
+{
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public string ProductDescription { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = null!;
+    }
+}
