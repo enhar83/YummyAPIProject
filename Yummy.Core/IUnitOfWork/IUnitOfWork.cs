8816c1745f0f355b34368247db81eb57b6112574
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-25 14:09:32 +0300

    IUoW ve UoW sınıfları oluşturuldu, metotlar içerisinde tanımlandı.

---
+++ Yummy.Core/IUnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Core.IUnitOfWork
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        Task<int> SaveAsync();
+    }
+}
---
+++ Yummy.Data/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yummy.Core.IUnitOfWork;
+using Yummy.Data.Context;
+
+namespace Yummy.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly YummyDbContext _context;
+
+        public UnitOfWork(YummyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+    }
+}
