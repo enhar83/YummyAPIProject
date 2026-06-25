a25272de68d7417ed7dd9de584a7ae9ab7e7d6f6
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-25 11:29:17 +0300

    Chef entitysi oluşturuldu içerisindeki propertyler tanımlandı.

---
+++ Yummy.Entity/Chef.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Entity
+{
+    public class Chef
+    {
+        public Guid ChefId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+    }
+}
