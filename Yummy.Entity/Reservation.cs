d0b1dd6ac996c21fbc141fc628f593f6c0d525d4
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-25 11:31:30 +0300

    Reservation entitysi oluşturuldu içerisinde propertyler tanımlandı.

---
+++ Yummy.Entity/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Entity
+{
+    public class Reservation
+    {
+        public Guid ReservationId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public DateTime ReservationDate { get; set; }
+        public string ReservationTime { get; set; } = null!;
+        public int NumberOfGuests { get; set; }
+        public string Message { get; set; } = null!;
+        public string ReservationStatus { get; set; } = null!;
+    }
+}
