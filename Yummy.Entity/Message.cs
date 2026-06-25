0cf45860b5310a2cb245fd92496c62848036bc5b
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-25 11:37:28 +0300

    Message entitysi oluşturuldu içerisinde propertyler tanımlandı.

---
+++ Yummy.Entity/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Entity
+{
+    public class Message
+    {
+        public Guid MessageId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Subject { get; set; } = null!;
+        public string MessageDetails { get; set; } = null!;
+        public DateTime SendDate { get; set; } 
+        public bool IsRead { get; set; }
+    }
+}
