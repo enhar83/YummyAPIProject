24a30dd6b5c11a81c423186802de911e1c190681
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-25 11:25:18 +0300

    Category entitysi oluşturuldu içerisinde propertyler tanımlandı.

---
+++ Yummy.Entity/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Entity
+{
+    public class Category
+    {
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; } = null!;
+    }
+}
