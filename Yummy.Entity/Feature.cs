15cb1d16ee68b948199b737acc06586666214626
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-25 11:22:05 +0300

    Feature entitysi oluşturuldu içerisinde propertyler tanımlandı.

---
+++ Yummy.Entity/Feature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Entity
+{
+    public class Feature
+    {
+        public Guid FeatureId { get; set; }
+        public string Title { get; set; } = null!;
+        public string SubTitle { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string VideoUrl { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+    }
+}
