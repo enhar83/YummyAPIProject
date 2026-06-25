0c302d65e92b4bdd5e45270185076a97d791519a
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-25 17:15:55 +0300

    ChefsController oluşturuldu, içerisinde actionlar tanımlandı.

---
+++ Yummy.WebAPI/Controllers/ChefsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Yummy.Core.DTOs.CategoryDTOs;
+using Yummy.Core.DTOs.ChefDTOs;
+using Yummy.Core.Services;
+
+namespace Yummy.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ChefsController : ControllerBase
+    {
+        private readonly IChefService _chefService;
+
+        public ChefsController(IChefService chefService)
+        {
+            _chefService = chefService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var chefs = await _chefService.GetAllAsync();
+            return Ok(chefs);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var chef = await _chefService.GetByIdAsync(id);
+            return Ok(chef);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromForm] ChefCreateDto dto)
+        {
+            await _chefService.AddAsync(dto);
+            return StatusCode(201, "Şef başarıyla oluşturuldu.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromForm] ChefUpdateDto dto)
+        {
+            await _chefService.UpdateAsync(dto);
+            return Ok("Şef başarıyla güncellendi.");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _chefService.DeleteAsync(id);
+            return Ok("Şef başarıyla silindi.");
+        }
+    }
+}
