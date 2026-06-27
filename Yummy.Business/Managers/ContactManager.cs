2d91884580aed212d450c123f25eaaeb1e3e942a
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-27 10:30:44 +0300

    IContactService ve ContactManager oluşturuldu, içerisinde metotlar tanımlandı. Program.cs kayıtları yapıldı.

---
+++ Yummy.Business/Managers/ContactManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Yummy.Core.DTOs.ContactDTOs;
+using Yummy.Core.Exceptions;
+using Yummy.Core.IRepositories;
+using Yummy.Core.IUnitOfWork;
+using Yummy.Core.Services;
+using Yummy.Entity;
+
+namespace Yummy.Business.Managers
+{
+    public class ContactManager : IContactService
+    {
+        private readonly IGenericRepository<Contact> _contactRepository;
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+
+        public ContactManager(IGenericRepository<Contact> contactRepository, IUnitOfWork uow, IMapper mapper)
+        {
+            _contactRepository = contactRepository;
+            _uow = uow;
+            _mapper = mapper;
+        }
+
+        public async Task AddAsync(ContactCreateDto dto)
+        {
+            var contact = _mapper.Map<Contact>(dto);
+            var isContactExist = await _contactRepository.GetSingleAsync(c => c.Address == dto.Address && c.Email == dto.Email && c.Phone == dto.Phone);
+            if (isContactExist != null)
+                throw new LogicException("ContactId", "Bu iletişim bilgileri zaten kullanılıyor.");
+
+            contact.ContactId = Guid.NewGuid();
+            await _contactRepository.AddAsync(contact);
+            await _uow.SaveAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var contact = await _contactRepository.GetByIdAsync(id);
+            if (contact == null)
+                throw new LogicException("ContactId", "Bu iletişim bilgileri bulunamadı.");
+
+            _contactRepository.Remove(contact);
+            await _uow.SaveAsync();
+        }
+
+        public async Task<IEnumerable<ContactResponseDto>> GetAllAsync()
+        {
+            var contacts = await _contactRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<ContactResponseDto>>(contacts);
+        }
+
+        public async Task<ContactResponseDto?> GetByIdAsync(Guid id)
+        {
+            var contact = await _contactRepository.GetByIdAsync(id);
+            if (contact == null)
+                throw new LogicException("ContactId", "Bu iletişim bilgileri bulunamadı.");
+
+            return _mapper.Map<ContactResponseDto>(contact);
+        }
+
+        public async Task UpdateAsync(ContactUpdateDto dto)
+        {
+            var contact = await _contactRepository.GetByIdAsync(dto.ContactId);
+            if (contact == null)
+                throw new LogicException("ContactId", "Bu iletişim bilgileri bulunamadı.");
+
+            var isContactExist = await _contactRepository.GetSingleAsync(c => c.Address == dto.Address &&
+                c.Email == dto.Email &&
+                c.Phone == dto.Phone &&
+                c.ContactId != dto.ContactId);
+            if (isContactExist != null)
+                throw new LogicException("ContactId", "Bu iletişim bilgileri zaten kullanılıyor.");
+
+            _mapper.Map(dto, contact);
+            _contactRepository.Update(contact);
+            await _uow.SaveAsync();
+        }
+    }
+}
---
+++ Yummy.Core/Services/IContactService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yummy.Core.DTOs.ContactDTOs;
+
+namespace Yummy.Core.Services
+{
+    public interface IContactService
+    {
+        Task<IEnumerable<ContactResponseDto>> GetAllAsync();
+        Task<ContactResponseDto?> GetByIdAsync(Guid id);
+        Task AddAsync(ContactCreateDto dto);
+        Task UpdateAsync(ContactUpdateDto dto);
+        Task DeleteAsync(Guid id);
+    }
+}
