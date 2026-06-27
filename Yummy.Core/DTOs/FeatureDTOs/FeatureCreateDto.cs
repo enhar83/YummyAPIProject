2f7a0c3d1ddc788a2d1d740b84673103f5a06280
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-27 11:10:13 +0300

    Feature entity'si için gerekli olan DTO'lar oluşturuldu.

---
+++ Yummy.Core/DTOs/FeatureDTOs/FeatureCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Core.DTOs.FeatureDTOs
+{
+    public class FeatureCreateDto
+    {
+        public string Title { get; set; } = null!;
+        public string SubTitle { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string VideoUrl { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+    }
+}
