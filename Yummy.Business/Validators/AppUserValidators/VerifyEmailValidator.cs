5fce8eb24ae3d5ef3eb8557b7cbd97c85e6b7325
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-27 22:23:35 +0300

    VerifyEmailDto oluşturuldu ve validator kodları tamamlandı.

---
+++ Yummy.Business/Validators/AppUserValidators/VerifyEmailValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Yummy.Core.DTOs.AppUserDTOs;
+
+namespace Yummy.Business.Validators.AppUserValidators
+{
+    public class VerifyEmailValidator : AbstractValidator<VerifyEmailDto>
+    {
+        public VerifyEmailValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("E-posta adresi boş geçilemez.")
+                .EmailAddress().WithMessage("Lütfen geçerli bir e-posta adresi giriniz.");
+
+            RuleFor(x => x.ActivationCode)
+                .NotEmpty().WithMessage("Aktivasyon kodu boş geçilemez.")
+                .Length(6).WithMessage("Aktivasyon kodu tam olarak 6 karakter olmalıdır.");
+        }
+    }
+}
---
+++ Yummy.Core/DTOs/AppUserDTOs/VerifyEmailDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Core.DTOs.AppUserDTOs
+{
+    public class VerifyEmailDto
+    {
+        public string Email { get; init; } = null!;
+        public string ActivationCode { get; init; } = null!;
+    }
+}
