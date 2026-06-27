dd069c3cdf4116917fa8247771e132b486b25184
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-27 10:10:30 +0300

    Contact entitysi için DTO'lar oluşturuldu.

---
+++ Yummy.Core/DTOs/ContactDTOs/ContactCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Core.DTOs.ContactDTOs
+{
+    public class ContactCreateDto
+    {
+        public string MapLocation { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string OpenHours { get; set; } = null!;
+    }
+}
---
+++ Yummy.Core/DTOs/ContactDTOs/ContactUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Core.DTOs.ContactDTOs
+{
+    public class ContactUpdateDto
+    {
+        public Guid ContactId { get; set; }
+        public string MapLocation { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string OpenHours { get; set; } = null!;
+    }
+}
