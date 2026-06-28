c48fa5a3f9f81b8aca8d36644cd78757ba08cd03
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-28 18:05:25 +0300

    Core/Settings içerisine JwtSettings oluşturuldu ve user secrets içerisinde jwt bilgileri kaydedildi.

---
+++ Yummy.Core/Settings/JwtSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Core.Settings
+{
+    public class JwtSettings
+    {
+        public string Audience { get; init; } = null!;
+        public string Issuer { get; init; } = null!;   
+        public int AccessTokenExpiration { get; init; } 
+        public string SecurityKey { get; init; } = null!;
+    }
+}
