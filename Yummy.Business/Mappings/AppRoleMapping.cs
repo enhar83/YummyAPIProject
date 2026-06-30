21bd1ef1a515f53a1595f8ae46f96bbe4ca5c054
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-06-30 10:14:53 +0300

    AppRoleCreateDto oluşturuldu, mapping ve validator kodları yazıldı.

---
+++ Yummy.Business/Mappings/AppRoleMapping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Yummy.Core.DTOs.AppRoleDTOs;
+using Yummy.Entity;
+
+namespace Yummy.Business.Mappings
+{
+    public class AppRoleMapping : Profile
+    {
+        public AppRoleMapping() 
+        {
+            CreateMap<AppRoleCreateDto, AppRole>();
+        }
+    }
+}
---
+++ Yummy.Core/DTOs/AppRoleDTOs/AppRoleCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yummy.Core.DTOs.AppRoleDTOs
+{
+    public class AppRoleCreateDto
+    {
+        public string Name { get; init; } = null!;
+        public string Description { get; init; } = null!;
+    }
+}
